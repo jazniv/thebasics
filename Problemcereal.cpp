3b3b044faea1713ddaaa461cf035fa362e2ab193
Juliana Glodek <64815206+jazniv@users.noreply.github.com>
2021-01-07 15:25:54 -0600

    Update Problemcereal.cpp

--- Problemcereal.cpp
+++ Problemcereal.cpp
@@ -11,9 +11,11 @@ int main(int argc, char **argv) {
   cout << "Enter the weight of the cereal in ounces:" << endl;
   cin >> cerealWeight;
 
+  // calculates weight in metric tons
   metricWeight = cerealWeight / metricTon;
   cout << "Cereal's Weight in Metric Tons: " << metricWeight << endl;
 
+  // calculates number of boxes needed for one metric ton
   numBoxes = metricTon / metricWeight;
   cout << "Number of Boxes Needed for One Metric Ton: " << numBoxes << endl;
 
